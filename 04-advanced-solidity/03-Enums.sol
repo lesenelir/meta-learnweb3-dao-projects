ff0a304b7031d5fb0016499ec834f289e1d11714
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-02 01:00:17 +0800

    feat: advanced solidity

---
+++ 04-advanced-solidity/03-Enums.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+contract 02-Enums {
+    function 02-Enums(){
+
+    }
+}
---
+++ 04-advanced-solidity/04-Structs.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+contract 04-Structs {
+    function 04-Structs(){
+
+    }
+}
