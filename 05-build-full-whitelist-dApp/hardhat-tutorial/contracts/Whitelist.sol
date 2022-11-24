eaf1b6b9b337f719ddcc2ceb5cb54fbff44343d0
lesenelir <miaomiaobabyzy@gmail.com>
2022-11-25 00:29:07 +0800

    feat: complete whitelist solidity hardhat smart contract

--- 05-build-full-whitelist-dApp/hardhat-tutorial/contracts/Whitelist.sol
+++ 05-build-full-whitelist-dApp/hardhat-tutorial/contracts/Whitelist.sol
@@ -2,5 +2,35 @@
 pragma solidity ^0.8.0;
 
 contract Whitelist {
+    // state variables
+    // max number of whitelisted address allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // mapping: address => bool
+    // if address is in whitelist, set it to true
+    // default value is false, for all addresses
+    mapping(address => bool) public whitelistedAddresses;
+
+    // keep track of how many addresses have been in whitelist
+    uint8 public numAddressesWhitelisted;
+
+    // contract called to set the max number of whitelisted address
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+        add address of sender to the whitelist
+    */
+    function addAddressToWhitelist() public {
+        // check user whether in whitelisted ?
+        require(!whitelistedAddresses[msg.sender], 'Sender has already been whitelisted');
+        // check the number of numAddressesWhitelisted < maxWhitelistedAddresses
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, 'Limit! No more address can be added!');
+        // add address in whitelist
+        whitelistedAddresses[msg.sender] = true;
+        // increase the num of whitelist addresses
+        numAddressesWhitelisted += 1;
+    }
 
 }
--- 05-build-full-whitelist-dApp/hardhat-tutorial/scripts/deploy.js
+++ 05-build-full-whitelist-dApp/hardhat-tutorial/scripts/deploy.js
@@ -1,31 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// 部署 deploy
+
+const {ethers} = require('hardhat')
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // A ContractFactory in ethers is an abstraction used to deploy new smart contract
+  const whitelistContract = await ethers.getContractFactory('Whitelist')
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // deploy the contract
+  // the number of 10 is the max number of address in whitelist
+  const deployedWhitelistContract = await whitelistContract.deploy(10)
 
-  await lock.deployed();
+  // 等待部署
+  await deployedWhitelistContract.deployed()
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log('Whitelist Contract Address: ', deployedWhitelistContract.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
